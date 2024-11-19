5d362c46706ba2ca125a1745d24c2dcddf5432a3
Harshit-Aggarwal9 <harshit.aggarwal@scaler.com>
2024-11-19 15:09:23 +0530

    initial push

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,4 +23,8 @@ export const SNAPSHOT_SCREENSHOT_FREQUENCY = 5000;
 export const DEFAULT_SNAPSHOT_RESIZE_OPTIONS = { width: 480, height: 360 };
 export const DEFAULT_SCREENSHOT_RESIZE_OPTIONS = { width: 480, height: 270 };
 export const MAX_EVENTS_BEFORE_SEND = 5;
-export const DEFAULT_HEADERS_CONTENT_TYPE = 'application/x-www-form-urlencoded';
+export const HEADER_CONTENT_TYPES = {
+  urlEncoded: 'application/x-www-form-urlencoded',
+  json: 'application/json',
+};
+export const BROWSER_BLUR_WARNING = 'Your test screen is not in focus. Please do not change your window or move your cursor outside the test screen. Doing so will lead to disqualification';
